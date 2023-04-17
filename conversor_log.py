c8910e8bfb9ccba4848fb2ddc947c6fd601e7c12
Kristopherk13 <kristo0120@gmail.com>
2023-04-17 09:42:07 -0500

    verificar ciclo For para datos intermedios

---
+++ conversor_log.py
@@ -0,0 +1,77 @@
+from tkinter import filedialog, messagebox, ttk
+from tkinter import *
+from getLocationPos import get_pos
+import os.path as path
+import os
+
+# Se crean las variables para llevar conteo de los puntos
+counter_p = 0
+counter_fp = 0
+counter_sp = 0
+counter_tp = 0
+
+
+
+
+raiz = Tk()
+raiz.title('Procesamiento archivos Sonar .log ANKA')
+raiz.resizable(1, 1)
+raiz.iconbitmap("anka.ico")
+ruta_archivo_log = None
+ruta_archivo_pos = None
+raiz.geometry("400x400")
+s = ttk.Style(raiz)
+s.layout("LabeledProgressbar",
+    [('LabeledProgressbar.trough',
+    {'children': [('LabeledProgressbar.pbar',
+                    {'side': 'left', 'sticky': 'ns'}),
+                    ("LabeledProgressbar.label",
+                    {"sticky": ""})],
+    'sticky': 'nswe'})])
+progressbar=ttk.Progressbar(raiz, orient="horizontal", length=300, mode="determinate", style="LabeledProgressbar")
+progressbar.pack(side=BOTTOM)
+
+def weeksecondstoutc(gpsweek,gpsseconds):
+    import datetime
+    datetimeformat = "%Y-%m-%d %H:%M:%S"
+    epoch = datetime.datetime.strptime("1980-01-06 00:00:00",datetimeformat)
+    elapsed = datetime.timedelta(days=(gpsweek*7),seconds=(gpsseconds))
+    return datetime.datetime.timestamp(epoch + elapsed)
+
+def state_check ():
+        check= var1.get()
+        # print (check)
+
+
+
+def abrir_archivo_pos():
+    global ruta_archivo_pos
+    ruta_archivo_pos = ""
+    lid_file_label = Label(text="Selecciono el archivo: ")
+    ruta_archivo_pos = filedialog.askopenfilename(initialdir = os.getcwd(),
+        title = "Seleccione archivo .pos", filetypes = (("Archivos binarios", ".pos"),
+        ("all files", "*.*")))
+    lid_file_label.place(x = 10, y = 160)
+    Label(text=ruta_archivo_pos).place(x = 10, y = 180)
+    print("Ruta del archivo .log: " + ruta_archivo_pos)
+
+# Funcion que se activa al pulsar el boton de "Abrir archivo" para cargar el archivo de trayectorias
+def convertir_archivo_button():
+    get_pos(ruta_archivo_pos=ruta_archivo_pos,check= var1.get())
+    info_message = "Los archivos han sido procesados correctamente"
+    messagebox.showinfo(title="Exito!!!", message=info_message)
+    
+
+
+
+Label(text="Seleccione el archivo .pos de la ruta").place(x = 10, y = 110)
+
+Button(text="Abrir archivo", bg="#eb7434", command = abrir_archivo_pos).place(x = 10, y = 130)
+
+Button(text="Convertir archivo", bg="#eb7434", command = convertir_archivo_button).place(x = 10, y = 205)
+
+var1 = IntVar()
+Checkbutton(text="archivo .pos procesado como backward",variable=var1, onvalue=1, offvalue=0,command = state_check).place(x = 10, y = 300)
+
+
+raiz.mainloop()
---
+++ getLocationPos.py
@@ -0,0 +1,109 @@
+from datetime import datetime, date, time
+import os
+from tkinter.constants import N
+import os.path as path
+
+timeStampPos = []
+nLocations = 0
+ruta_archivo_pos = ""
+posFreq = 1
+var1 = 0
+
+buffer_gtm = []
+
+def get_pos(ruta_archivo_pos, check):
+    ruta_salida = path.abspath(path.join(ruta_archivo_pos,".."))
+    fsuperpos = open(ruta_salida + "/superpos.pos","w+")
+    count = 0
+    check = check
+
+    secondsA = ""
+    coordinatesA = ""
+    coordinatesA1 = ""
+    altitudA = ""
+    secondsB = ""
+    coordinatesB = ""
+    coordinatesB1= ""
+    altitudB = ""
+
+
+    global timeStampPos
+    global nLocations
+    timeStamp = []
+    nLocations = 0
+    pos_size = os.path.getsize(ruta_archivo_pos)
+    with open(ruta_archivo_pos, "r") as file_object:
+        wait = True
+        while wait:
+            buffer = file_object.readline()
+            if len(buffer) != 0:
+                if buffer[0:7] == "%  GPST":
+                    fsuperpos.write("%  GPST                  latitude(deg) longitude(deg)  height(m)   Q  ns   sdn(m)   sde(m)   sdu(m)  sdne(m)  sdeu(m)  sdun(m) age(s)  ratio\n")
+                    buffer = "0"
+                    while wait:
+                        if  check == 0:
+                            if buffer == "0":
+                                buffer = file_object.readline()
+                            else:
+                                buffer = buffer2
+                            buffer2= file_object.readline()
+                            if len(buffer2) != 0:
+                                data_process(buffer, buffer2 ,fsuperpos )
+                                
+                            else:
+                                fsuperpos.write(buffer)
+                                wait = False
+                            
+                        else:
+                            lines = file_object.readlines()
+                            for buffer2 in reversed(lines):
+                                if buffer == "0":
+                                    buffer = file_object.readline()
+                                else:
+                                    buffer = buffer2
+                                buffer2= file_object.readline()
+                                if len(buffer2) != 0:
+                                    data_process(buffer, buffer2 ,fsuperpos )
+                                else:
+                                    fsuperpos.write(buffer)
+                                    wait = False
+                            wait = False
+                else:
+                    if len(buffer) != 0:
+                        fsuperpos.write(buffer)
+
+            else:
+                wait = False
+    fsuperpos.close()
+    
+    if nLocations != 0:
+        sorted(timeStamp, key=lambda x: x[0])
+        timeStampPos = tuple(timeStamp)
+    file_object.close()
+
+
+
+def data_process(buffer, buffer2 ,fsuperpos ):
+    secondsA = buffer[17:19]
+    coordinatesA = buffer[25:38]
+    coordinatesA1 = buffer[40:53]
+    altitudA = buffer[56:64]
+
+    secondsB = buffer2[17:19]
+    coordinatesB = buffer2[25:38]
+    coordinatesB1= buffer2[40:53]
+    altitudB = buffer2[56:64]
+    
+    fsuperpos.write(buffer)
+
+    secondsA = ((int(secondsA) - int(secondsB))/4)+0.001
+    secondsB = str(secondsA)
+    if len(secondsB) <= 5:
+            secondsB = "0"+secondsB
+    coordinatesB = str(eval("(" + coordinatesA + "+" + coordinatesB + ")/2"))
+    coordinatesB1 = str(eval("(" + coordinatesA1 + "+" + coordinatesB1 + ")/2"))
+    altitudB = str(eval("(" + altitudA + "+" + altitudB + ")/2"))
+    # buffer_gtm[i] = buffer
+    fsuperpos.write(buffer[0:17] + secondsB[0:2] + "." + secondsB[3:6] + buffer[23:25] + coordinatesB[0:13] + "  " + coordinatesB1[0:13] + "   " + altitudB[0:8] + buffer[64:142])
+
+# fsuperpos.write(buffer2)
